c6a4d63640452b7196f95fa773f23e0c1a6998b0
ateworld9 <vahrameev.work@gmail.com>
2020-08-27 22:04:36 +0300

    release 2. add return picture, add client side rendering

--- routes/gameRouter.js
+++ routes/gameRouter.js
@@ -5,26 +5,36 @@ const router = express.Router();
 
 router.route('/')
   .get(async (req, res) => {
-    let deck1 = await Card.find({ deckTitle: 'deck1' });
+    const deck1 = await Card.find({ deckTitle: 'deck1' });
     // console.log(deck1);
-    let { picturePath1 } = deck1[0];
-    let { picturePath2 } = deck1[0];
-    let { coverPath } = deck1[0]
-    let deck2 = await Card.find({ deckTitle: 'deck2' });
-    let picturePath12 = deck2[0].picturePath1;
-    let picturePath22 = deck2[0].picturePath2;
-    let coverPath2 = deck2[0].coverPath;
-    let deck2Id = deck2[0]._id.toString();
+    const { picturePath1 } = deck1[0];
+    const { picturePath2 } = deck1[0];
+    const { coverPath } = deck1[0];
+    const deck1Id = deck1[0]._id.toString();
+    const deck2 = await Card.find({ deckTitle: 'deck2' });
+    const picturePath12 = deck2[0].picturePath1;
+    const picturePath22 = deck2[0].picturePath2;
+    const coverPath2 = deck2[0].coverPath;
+    const deck2Id = deck2[0]._id.toString();
     // console.log(typeof deck2Id);
-    res.render('gameGround', { picturePath1, picturePath2, coverPath, picturePath12, picturePath22, coverPath2, deck2Id })
+    res.render('gameGround', {
+      picturePath1,
+      picturePath2,
+      coverPath,
+      picturePath12,
+      picturePath22,
+      coverPath2,
+      deck2Id,
+      deck1Id,
+    });
   });
 
 router.get('/:id', async (req, res) => {
-  let deck = await Card.find({ _id: req.params.id });
+  const deck = await Card.find({ _id: req.params.id });
   // console.log(deck);
-  let { picturePath1, picturePath2 } = deck[0];
+  const { picturePath1, picturePath2 } = deck[0];
   // console.log(picturePath1, picturePath2);
-  res.json({ picturePath1, picturePath2 })
-})
+  res.json({ picturePath1, picturePath2 });
+});
 
 export default router;
