b7cc3886222f04e5ed664a45d5e792afa92b01b0
ateworld9 <vahrameev.work@gmail.com>
2020-08-27 22:44:54 +0300

    heroku fix bug

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use('/game', gameRouter);
 
 useErrorHandlers(app);
 
-const port = process.env.PORT ?? 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`${port} is alive!`);
   try {
