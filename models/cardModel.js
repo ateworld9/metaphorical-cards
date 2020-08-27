c58b22d2f5b3f9c16642f4ca492e07d7a7735b9c
Алена <alena.lubanko@yandex.ru>
2020-08-27 15:29:28 +0300

    model deck3

--- models/cardModel.js
+++ models/cardModel.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const cardSchema = mongoose.Schema({
     deckTitle: String,
     coverPath: String,
-    picturePath: Array
+    picturePath1: Array,
+    picturePath2: Array
 });
 
 export default mongoose.model("cards", cardSchema);
